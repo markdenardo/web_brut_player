75d607bef38a94f10599cb5f4248b545521929ad
markdenardo <mark.denardo@gmail.com>
2020-04-29 00:19:16 -0400

    Updates

---
+++ precache-manifest.0234da2cef6f37389c3b7c1a7048e102.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f52953782e6cb2c7c8330c5ea948c5f6",
+    "url": "/web_brut_player/index.html"
+  },
+  {
+    "revision": "a632937e8d0e3fc1f20c",
+    "url": "/web_brut_player/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9ea5980bbf3cf6e635b9",
+    "url": "/web_brut_player/static/js/2.ea9408ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web_brut_player/static/js/2.ea9408ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a632937e8d0e3fc1f20c",
+    "url": "/web_brut_player/static/js/main.ef72fce3.chunk.js"
+  },
+  {
+    "revision": "8ba79e08f17f60369901",
+    "url": "/web_brut_player/static/js/runtime-main.fef9801c.js"
+  }
+]);
